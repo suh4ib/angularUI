3c985467ce2f20bf4dd71570b197068319ee6ecb
karan <kmachado618@gmail.com>
2020-08-13 14:43:49 -0400

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,66 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { DialogModule } from 'primeng/dialog';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { TabViewModule } from 'primeng/tabview';
+import { CodeHighlighterModule } from 'primeng/codehighlighter';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { KeyFilterModule } from 'primeng/keyfilter';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { CheckboxModule } from 'primeng/checkbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeHomeComponent } from './employee-home/employee-home.component';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeHomeComponent } from './employee-home/employee-home.component';
+import { TableModule } from 'primeng/table';
+import { SidebarModule } from 'primeng/sidebar';
 import { EmployeeAttendanceComponent } from './employee-attendance/employee-attendance.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeHomeComponent,
     EmployeeFormComponent,
     EmployeeListComponent,
-    EmployeeAttendanceComponent
+    EmployeeHomeComponent,
+    EmployeeAttendanceComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    DialogModule,
+    CalendarModule,
+    DropdownModule,
+    InputMaskModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    ButtonModule,
+    TabViewModule,
+    TableModule,
+    CodeHighlighterModule,
+    InputTextModule,
+    RadioButtonModule,
+    SelectButtonModule,
+    KeyFilterModule,
+    InputNumberModule,
+    InputSwitchModule,
+    CheckboxModule,
+    SidebarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
